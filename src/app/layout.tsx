a180b7227469da7ef3f9144c5e52ed557a25b14a
Zsuzsa.L <manoaway@gmail.com>
2023-12-20 10:36:37 +0100

    add header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
+import { Header } from "./layout/Header";
+import { Footer } from "./layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ src/app/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+export const Footer: FC = () => {
+  return (
+    <footer className="max-w-5xl mx-auto p-4">
+      <div className="border border-gray-700 my-6"></div>
+      <p className=" w-full text-center sm:text-right">
+        © {new Date().getFullYear()}
+        <span className=""> Zsuzsa Lukacs</span>
+      </p>
+    </footer>
+  );
+};
