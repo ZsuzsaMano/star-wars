f16dbf3f488685b0aeaae3517867a7388ffef913
Zsuzsa.L <manoaway@gmail.com>
2023-12-21 15:36:51 +0100

    if in squad remove from search results

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -20,42 +20,44 @@ export const Frame: FC<Frame> = ({ person, isSquad, setSquad, squad }) => {
       } else {
         /** in People component add characters up to 5 person */
         if (squad.length < 5)
-          setSquad((prevSquad) => [
-            ...(prevSquad || []),
-            { id: person.id, image: person.image },
-          ]);
+          setSquad((prevSquad) => [...(prevSquad || []), person]);
       }
   };
-  return (
-    <div
-      className={classNames(
-        "shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] w-36  rounded-md",
-        isSquad ? " w-20 h-36" : "w-36 h-56"
-      )}
-    >
-      <figure>
-        <div className="rounded-md relative">
-          <Image
-            src={person?.image || placeholderPic}
-            width={500}
-            height={500}
-            alt="Placeholder for character"
-            className="rounded-md hover:opacity-50 active:opacity-50"
-          />
-          <button
-            className="absolute top-1 left-4 w-10 h-10 text-5xl"
-            onClick={handleSquadChange}
-          >
-            {isSquad ? "-" : "+"}
-          </button>
-        </div>
-        <figcaption className="p-2 text-center">
-          <strong>{person?.name || ""}</strong>
-          <p className={classNames(isSquad && "hidden")}>
-            {person?.species?.name}
-          </p>
-        </figcaption>
-      </figure>
-    </div>
-  );
+
+  /** if person already in Squad dont show it in search results*/
+  if (!isSquad && squad?.filter((item) => item.id === person?.id).length != 0)
+    return null;
+  if (person)
+    return (
+      <div
+        className={classNames(
+          "shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] rounded-md",
+          isSquad ? "w-28" : "w-36 h-60"
+        )}
+      >
+        <figure>
+          <div className="rounded-md relative">
+            <Image
+              src={person.image || ""}
+              width={500}
+              height={500}
+              alt={person.name || ""}
+              className="rounded-md hover:opacity-50 active:opacity-50"
+            />
+            <button
+              className="absolute top-1 left-4 w-10 h-10 text-5xl"
+              onClick={handleSquadChange}
+            >
+              {isSquad ? "-" : "+"}
+            </button>
+          </div>
+          <figcaption className="p-2 text-center">
+            <strong>{person.name}</strong>
+            <p className={classNames(isSquad && "hidden")}>
+              {person.species?.name}
+            </p>
+          </figcaption>
+        </figure>
+      </div>
+    );
 };
