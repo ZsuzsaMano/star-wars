7c07df17d246c820ba36b6b8c6226222973c2f65
Zsuzsa.L <manoaway@gmail.com>
2023-12-23 19:20:49 +0100

    max 5 squad members

--- src/store/zustand.ts
+++ src/store/zustand.ts
@@ -9,7 +9,12 @@ type SquadStore = {
 
 export const useSquadStore = create<SquadStore>((set) => ({
   squad: [],
-  addToSquad: (person) => set((state) => ({ squad: [...state.squad, person] })),
+  addToSquad: (person) =>
+    set((state) =>
+      state.squad.length < 5
+        ? { squad: [...state.squad, person] }
+        : { squad: state.squad }
+    ),
   removeFromSquad: (person) =>
     set((state) => ({
       squad: state.squad.filter((p) => p.id !== person.id),
