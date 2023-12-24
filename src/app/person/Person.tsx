ef78148e3fc5379439ba274ebc5f2f66181b4274
Zsuzsa.L <manoaway@gmail.com>
2023-12-24 09:17:59 +0100

    add back button to person page

--- src/app/person/Person.tsx
+++ src/app/person/Person.tsx
@@ -4,12 +4,18 @@ import Image from "next/image";
 import { GET_PERSON } from "@/graphQL/queries";
 import { PersonQueryProps } from "@/types/shared.types";
 import { Loader } from "@/components/Loader";
+import { useRouter } from "next/navigation";
 
 export default function Person() {
+  /** get query from URL */
   const searchParams = useSearchParams();
+
+  /** get routing access */
+  const router = useRouter();
 
   const id = searchParams.get("id");
 
+  /** get the data of the person with the given id */
   const { data, error, loading } = useQuery<PersonQueryProps>(GET_PERSON, {
     variables: { personId: id },
   });
@@ -21,6 +27,7 @@ export default function Person() {
   if (person)
     return (
       <main className="min-h-screen max-w-5xl mx-auto p-4">
+        <button onClick={() => router.back()}>Back</button>
         <Image
           src={person.image || ""}
           width={500}
@@ -40,6 +47,7 @@ export default function Person() {
               </p>
             );
         })}
+        <button onClick={() => router.back()}>Back</button>
       </main>
     );
 }
