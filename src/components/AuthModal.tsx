6fd7aa150e1b2d7f1ab3b5667538317f7a0998a4
Zsuzsa.L <manoaway@gmail.com>
2023-12-26 21:39:42 +0100

    create signup from

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,58 +1,68 @@
-import React, { FC, Dispatch, SetStateAction, useState } from "react";
+import { SIGNUP } from "@/graphQL/mutations";
+import { useMutation } from "@apollo/client";
+import React, {
+  FC,
+  Dispatch,
+  SetStateAction,
+  useState,
+  FormEvent,
+} from "react";
 
 type AuthModalProps = {
   setShowModal: Dispatch<SetStateAction<boolean>>;
   showModal: boolean;
 };
 
-type FormStateProps = { isLogin: boolean; email: string; password: string };
-
 export const AuthModal: FC<AuthModalProps> = ({ setShowModal, showModal }) => {
-  const [formState, setFormState] = useState<FormStateProps>({
-    isLogin: true,
-    email: "",
-    password: "",
-  });
+  const [isLogin, setIsLogin] = useState<boolean>(true);
+
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    console.log("formData", formData);
+    // signUp({ variables: { formData } });
+  }
+
+  // const [signUp, { data, loading, error }] = useMutation(SIGNUP);
+
+  // if (loading) return "Submitting...";
+
+  // if (error) return `Submission error! ${error.message}`;
+
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
         <div className="relative w-auto my-6 mx-auto max-w-3xl">
           {/*content*/}
-          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+          <form
+            onSubmit={onSubmit}
+            className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none"
+          >
             {/*header*/}
             <div className="flex justify-between p-4 rounded-t">
-              <h3>{formState.isLogin ? "Login" : "Sign up"}</h3>
+              <h3>{isLogin ? "Login" : "Sign up"}</h3>
             </div>
             {/*body*/}
             <div className="flex flex-col p-6 gap-4">
               <input
                 className="p-2 text-sm w-full outline-2 placeholder:text-gray-500 rounded-md shadow-[1px_1px_2px_1px_#a2a2ad]"
                 placeholder="email"
-                onChange={(e) =>
-                  setFormState({
-                    ...formState,
-                    email: e.target.value,
-                  })
-                }
+                name="email"
               />
               <input
                 className="p-2 text-sm w-full outline-2 placeholder:text-gray-500 rounded-md shadow-[1px_1px_2px_1px_#a2a2ad]"
                 placeholder="password"
                 type="password"
-                onChange={(e) =>
-                  setFormState({
-                    ...formState,
-                    password: e.target.value,
-                  })
-                }
+                name="password"
               />
               <button
                 className="text-blue md:text-xs text-left"
                 onClick={() => {
-                  setFormState({ ...formState, isLogin: !formState.isLogin });
+                  setIsLogin(!isLogin);
                 }}
+                type="button"
               >
-                {formState.isLogin
+                {isLogin
                   ? "No account yet? Sign up!"
                   : "Already registered? Login!"}
               </button>
@@ -69,13 +79,12 @@ export const AuthModal: FC<AuthModalProps> = ({ setShowModal, showModal }) => {
               </button>
               <button
                 className="bg-blue text-white active:bg-emerald-600 font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                type="button"
-                onClick={() => setShowModal(false)}
+                type="submit"
               >
-                {formState.isLogin ? "Login" : "Sign up"}
+                {isLogin ? "Login" : "Sign up"}
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
       <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
