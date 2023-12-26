ba8c9f3e89f912d4dcc79e1848566e3ed97084f2
Zsuzsa.L <manoaway@gmail.com>
2023-12-26 22:09:04 +0100

    fix  img warning

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -6,7 +6,14 @@ export const Loader = () => {
     <div>
       Loading...
       <div className="flex items-center">
-        <Image src={hilt} width={60} height={60} alt="lightsaber hilt" />
+        <Image
+          src={hilt}
+          width={0}
+          height={0}
+          sizes="100vw"
+          alt="lightsaber hilt"
+          className="w-14"
+        />
         <div className="h-2 w-full flex items-center relative overflow-hidden  bg-[#ff0000] shadow-[7px_7px_14px_#ff0000,-7px_-7px_14px_#ff0000] rounded-r-full animate-[animate_4s_infinite]"></div>
       </div>
     </div>
