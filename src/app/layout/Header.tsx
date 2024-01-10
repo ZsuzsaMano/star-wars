0003b60091cf4f31be296179fb7da147f26a36e8
Zsuzsa.L <manoaway@gmail.com>
2024-01-10 10:10:14 +0100

    add home link to header title

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 import React, { FC } from "react";
 
 export const Header: FC = () => {
   return (
     <header className="max-w-5xl mx-auto md:p-4 text-center">
-      <h1>Star Wars Squad</h1>
+      <Link
+        href={{
+          pathname: "/",
+        }}
+      >
+        <h1>Star Wars Squad</h1>
+      </Link>
       <div className="border border-gray-700 mt-2 md:my-3"></div>
     </header>
   );
